d557772b2b7cc91263314400f25331edff44e665
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-22 12:05:07 +0100

    Made the SimpleGUI base. Needs to implement the action on the button save

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,11 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,5 +13,24 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
     private final JFrame frame = new JFrame();
+    private final Controller controller = new Controller();
 
+    public SimpleGUI() {
+        final JButton saveButton = new JButton("Save");
+        final JTextArea inputText = new JTextArea();
+        this.frame.setLayout(new BorderLayout());
+        this.frame.add(saveButton, BorderLayout.SOUTH);
+        this.frame.add(inputText, BorderLayout.CENTER);
+    }
+
+    private void display() {
+        frame.setLocationByPlatform(true);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String args[]) {
+        new SimpleGUI().display();
+    }
 }
