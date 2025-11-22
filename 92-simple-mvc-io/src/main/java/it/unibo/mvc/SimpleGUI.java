ce904885437eb928b8815081ff34f221f507f104
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-22 12:36:33 +0100

    Implemented the write action on the button. Now it correctly writes the text in the file. It could be enhanced by using a JDIalog to show a message if an IOException is thrown

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -5,7 +5,9 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * A very simple program using a graphical interface.
@@ -21,6 +23,18 @@ public final class SimpleGUI {
         this.frame.setLayout(new BorderLayout());
         this.frame.add(saveButton, BorderLayout.SOUTH);
         this.frame.add(inputText, BorderLayout.CENTER);
+
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    controller.writeStringToFile(inputText.getText());
+                } catch (IOException e1) {
+                    // This could be a JDIalog showing the error.
+                    System.out.println(e1.getStackTrace()); //NOMPD
+                }
+            }
+        });
     }
 
     private void display() {
