0894f0f02022fdb490bb6e3966f66e8e32ee043e
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-22 11:01:50 +0100

    Started implementation of controller. Used a PrintStream with the default charset to write on files.

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,39 @@
 package it.unibo.mvc;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private static final String DEFAULT_FILE_NAME = "output.txt";
+
+    private String filePath;
+    private final String systemSeparator;
+    private PrintStream file;  
+
+    public Controller() throws IOException {
+        this.systemSeparator = System.getProperty("file.separator");
+        this.filePath = System.getProperty("user.home");
+        this.file = new PrintStream(this.filePath + DEFAULT_FILE_NAME, StandardCharsets.UTF_8);
+    }
+
+    public void setCurrentFile() {
+
+    }
+
+    public PrintStream getCurrentFile() {
+        return this.file;
+    }
+
+    public String getCurrentFilePath() {
+        return "";
+    }
+
+    public void writeStringToFile() {
+
+    }
 
 }
