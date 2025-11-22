04580360c3d1d52ce3dd0236ebece888f2621b3f
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-22 18:02:47 +0100

    Made the controller interface and it's implementation for standard output

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,35 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
 
+    /**
+     * This function sets the new string to print.
+     * 
+     * @param nextString
+     */
+    void setNextString(final String nextString);
+    
+    /**
+     * This function return the actual set string.
+     *  
+     * @return actual set string.
+     */
+    String getNextString();
+
+    /**
+     * Returns a list with the string history.
+     * 
+     * @return a list that contains the printed string history.
+     */
+    List<String> getStringHistory();
+
+    /**
+     * Prints the current string in the standard output.
+     */
+    void printCurrentString();
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,77 @@
 package it.unibo.mvc;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * 
- *
+ * Class that manages the printing of a string in the standard output.
+ * Implements the {@link Controller} interface. 
  */
 public final class SimpleController implements Controller {
+    private static final String UNSET_STRING = ""; 
+    private String nextString;
+    private final List<String> history = new LinkedList<>();
 
+    /**
+     * Constructor with no arguments. It makes the string as unset by default.
+     */
+    public SimpleController() {
+        this.nextString = UNSET_STRING;
+    }
+
+    /**
+     * Constructor that takes a string to set as next to print.
+     * The setting is made by calling the {@link setNextString} method.
+     * 
+     * @param nextString the string to set as next to print.
+     */
+    public SimpleController(final String nextString) {
+        setNextString(nextString);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setNextString(String nextString) {
+        if (nextString != null) {
+            this.nextString = nextString;
+        } else {
+            throw new IllegalArgumentException("The string cannot have NULL values");
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getNextString() {
+        return this.nextString;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> getStringHistory() {
+        return this.history;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void printCurrentString() {
+        if (this.nextString == null || this.nextString == UNSET_STRING) {
+            throw new IllegalStateException("The next string cannot be unset");
+        } else {
+            System.out.println(this.nextString); //NOPMD
+            history.add(this.nextString);
+            unsetStringAfterPrint();
+        }
+    }
+
+    private void unsetStringAfterPrint() {
+        this.nextString = UNSET_STRING;
+    }
 }
